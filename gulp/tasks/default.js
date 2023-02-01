fc86ba8386d991fe4fe4a499f076d9795291c104
Robert St. John <restjohn@users.noreply.github.com>
2023-01-31 23:39:47 -0700

    changes from @ngageoint/mage-server to support combined date and time picking

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var config = require('../config');
 var browserSync = require('browser-sync');
 
-gulp.task('default', ['browserSyncTask'], function () {
+gulp.task('default', gulp.series('browserSyncTask'), function () {
   	gulp.watch(config.src.js, ['scripts', browserSync.reload]);
   	gulp.watch(config.src.scss, ['styles', browserSync.reload]);
   	gulp.watch(config.src.svg, ['images', browserSync.reload]);
